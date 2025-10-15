11047a6f7d524beb91303511a4082226f58ef7f9
王健 <wjb19891223@163.com>
2025-10-15 17:44:01 +0800

    维护模式

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { TRPCReactProvider } from "@/trpc/client";
-import "./globals.css";
-import { Toaster } from "sonner";
-import { NuqsAdapter } from "nuqs/adapters/next/app";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Meet.AI",
-  description: "Meet.AI 是一个基于 AI 的会议管理工具",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout() {
   return (
-    <NuqsAdapter>
-      <TRPCReactProvider>
-        <html lang="zh-CN">
-          <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-          >
-            <Toaster />
-            {children}
-          </body>
-        </html>
-      </TRPCReactProvider>
-    </NuqsAdapter>
+    <html lang="zh-CN">
+      <body>暂不可访问</body>
+    </html>
   );
 }
